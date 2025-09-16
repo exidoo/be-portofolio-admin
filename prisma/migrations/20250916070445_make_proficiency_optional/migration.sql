e02a98b7c93b622f011b921ed157e6e2c9d1a6d1
unknown <romirlajuardi@gmail.com>
2025-09-16 19:33:08 +0700

    Update & Fix error

---
+++ prisma/migrations/20250916070445_make_proficiency_optional/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "public"."Skill" ALTER COLUMN "proficiency" DROP NOT NULL;
---
+++ prisma/migrations/20250916070652_make_category_optional/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "public"."Skill" ALTER COLUMN "category" DROP NOT NULL;
